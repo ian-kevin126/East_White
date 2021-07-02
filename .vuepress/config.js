78ce10f8372ed6e6289c66502752b69e9ded5331
East_White_seu <ian_kevin126@126.com>
2021-07-02 15:45:06 +0800

    add files

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,6 +5,7 @@
 // See the documentation for more information on how to use it
 // https://v1.vuepress.vuejs.org/config/
 // 配置教程：https://www.moyundong.com/frontend/vuepress/1%E4%BB%8B%E7%BB%8D.html
+// Markdown语法：https://www.jianshu.com/p/191d1e21f7ed
 
 module.exports = {
   title: "East_White",
@@ -12,33 +13,57 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
-      { text: "HTML/CSS", link: "/html-css/" },
+      {
+        text: "HTML/CSS",
+        items: [
+          { text: "HTML", link: "/HTML/" },
+          { text: "CSS", link: "/CSS/" }
+        ]
+      },
       {
         text: "JS+ES+TS",
         items: [
-          { text: "JS", link: "/javascript/js/" },
-          { text: "ES", link: "/javascript/es/" },
-          { text: "TS", link: "/javascript/ts/" }
+          { text: "JavaScript", link: "/JavaScript/" },
+          { text: "ECMAScript", link: "/ECMAScript/" },
+          { text: "TypeScript", link: "/TypeScript/" }
         ]
       },
-      { text: "Webpack", link: "/webpack/" },
-      { text: "React", link: "/react/" },
-      { text: "Flutter", link: "/flutter/" },
-      { text: "Node", link: "/node/" },
+      { text: "Webpack", link: "/Webpack/" },
+      { text: "React", link: "/React/" },
+      { text: "Flutter", link: "/Flutter/" },
+      { text: "Node", link: "/Node/" },
       {
         text: "算法",
         items: [
-          {
-            text: "数据结构",
-            link: "/data-structure-and-algorithm/data-structure/"
-          },
-          { text: "算法", link: "/data-structure-and-algorithm/algorithm/" }
+          { text: "数据结构", link: "/DataStructures/" },
+          { text: "算法", link: "/Algorithms/" }
         ]
       },
       {
         text: "GitHub",
         link: "https://github.com/ian-kevin126"
       }
-    ]
+    ],
+    sidebar: {
+      "/DataStructures/": ["Array_1"],
+      "/Algorithms/": [
+        {
+          title: "排序算法",
+          collapsable: false,
+          children: [
+            {
+              title: "冒泡排序",
+              path: "/Algorithms/sort/bubble-sort.md"
+            },
+            {
+              title: "冒泡排序",
+              path: "/Algorithms/sort/bubble-sort.md"
+            }
+          ]
+        }
+      ]
+    },
+    sidebarDepth: 2, //左侧导航显示的层级
+    lastUpdated: "Last Updated"
   }
 };
