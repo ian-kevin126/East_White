27fa75385996ab5eb41c4db74702a06b7e998ff4
East_White_seu <ian_kevin126@126.com>
2021-07-02 14:00:26 +0800

    feat add project structure

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,6 +4,7 @@
 // It can also be written in yml or toml instead of js
 // See the documentation for more information on how to use it
 // https://v1.vuepress.vuejs.org/config/
+// 配置：https://www.moyundong.com/frontend/vuepress/4%E5%9F%BA%E6%9C%AC%E9%85%8D%E7%BD%AE.html#config-js%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6
 
 module.exports = {
   title: "East_White",
@@ -11,11 +12,22 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
-      { text: "JavaScript", link: "/guide/" },
-      { text: "TypeScript", link: "https://vuepress.vuejs.org/" },
-      { text: "Webpack", link: "https://vuepress.vuejs.org/" },
-      { text: "React", link: "https://vuepress.vuejs.org/" },
-      { text: "Node", link: "https://vuepress.vuejs.org/" },
+      { text: "JavaScript", link: "/javascript/" },
+      { text: "TypeScript", link: "/typescript/" },
+      { text: "Webpack", link: "/webpack/" },
+      { text: "React", link: "/react/" },
+      { text: "Flutter", link: "/flutter/" },
+      { text: "Node", link: "/node/" },
+      {
+        text: "算法",
+        items: [
+          {
+            text: "数据结构",
+            link: "/data-structure-and-algorithm/data-structure/"
+          },
+          { text: "算法", link: "/data-structure-and-algorithm/algorithm/" }
+        ]
+      },
       {
         text: "GitHub",
         link: "https://github.com/ian-kevin126"
