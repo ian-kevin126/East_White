3ea06727a84788731c46aba9c87308a8ea1a2fa8
East_White_seu <ian_kevin126@126.com>
2021-07-05 08:56:57 +0800

    fix: solve conflict

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,27 +7,14 @@
 // 配置教程：https://www.moyundong.com/frontend/vuepress/1%E4%BB%8B%E7%BB%8D.html
 // Markdown语法：https://www.jianshu.com/p/191d1e21f7ed
 
-const sidebarItem = (title, path, depth = 2) => {
-  return {
-    title: title,
-    sidebarDepth: depth,
-    path: path,
-    collapsable: true
-  };
-};
-
 module.exports = {
   title: "East_White",
   description: "The road ahead is vast and expectable!",
   themeConfig: {
-    smoothScroll: true,
-    editLinks: true,
-    sidebarDepth: 2, //左侧导航显示的层级
-    lastUpdated: "Last Updated",
     nav: [
       { text: "Home", link: "/" },
       {
-        text: "HTML&CSS",
+        text: "HTML/CSS",
         items: [
           { text: "HTML", link: "/HTML/" },
           { text: "CSS", link: "/CSS/" }
@@ -58,10 +45,6 @@ module.exports = {
       }
     ],
     sidebar: {
-      "/HTML/": [sidebarItem("HTML_1", "/HTML/html_1")],
-      "/DataStructures/": [
-        sidebarItem("Array1111", "/DataStructures/Array/Array_1")
-      ],
       "/Webpack/": [
         {
           title: "webpack",
@@ -69,6 +52,8 @@ module.exports = {
           childres: [{ title: "1111", path: "/Webpack/111" }]
         }
       ]
-    }
+    },
+    sidebarDepth: 2, //左侧导航显示的层级
+    lastUpdated: "Last Updated"
   }
 };
